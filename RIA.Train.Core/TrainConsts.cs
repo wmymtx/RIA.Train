00316dc1f338c31e49d2f412df2b21b4f8287f8f
wmymtx <wmymtx@163.com>
2017-05-21 07:03:23 +0800

    new code

---
+++ RIA.Train.Core/TrainConsts.cs
@@ -0,0 +1,9 @@
+﻿namespace RIA.Train
+{
+    public class TrainConsts
+    {
+        public const string LocalizationSourceName = "听课系统";
+
+        public const bool MultiTenancyEnabled = false;
+    }
+}
